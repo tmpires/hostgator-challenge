b8ae75789c4d885a9e3ae706f73a78d77fc26685
Thais Pires <tmendespires@gmail.com>
2020-10-22 17:24:28 -0300

    header and banner complete

--- src/pages/Plans/index.tsx
+++ src/pages/Plans/index.tsx
@@ -1,12 +1,63 @@
 import React from 'react';
 
-import { Container, Content } from './styles';
+import { FiCheck } from 'react-icons/fi';
+import {
+  Container,
+  Content,
+  Header,
+  HeaderContent,
+  Banner,
+  BannerContent,
+  BannerText,
+  Wave,
+} from './styles';
+
+import logoImg from '../../assets/hostgator-logo.svg';
+import deskImg from '../../assets/Grupo-29995.svg';
+import customerImg from '../../assets/Grupo-29996.svg';
 
 const SignIn: React.FC = () => {
   return (
     <Container>
+      <Header>
+        <HeaderContent>
+          <img src={logoImg} alt="GoBarber" />
+        </HeaderContent>
+      </Header>
+      <Banner>
+        <BannerContent>
+          <img src={deskImg} alt="Desk" />
+          <BannerText>
+            <span>Hospedagem de Sites</span>
+            <h2>
+              Tenha uma hospedagem de sites estável e evite perder visitantes
+              diariamente
+            </h2>
+            <span>
+              <FiCheck />
+              99,9% de disponibilidade: seu site sempre no ar
+            </span>
+            <span>
+              <FiCheck />
+              Suporte 24h, todos os dias
+            </span>
+            <span>
+              <FiCheck />
+              Painel de Controle cPanel
+            </span>
+          </BannerText>
+          <img src={customerImg} alt="Customer" />
+        </BannerContent>
+        <Wave>
+          <svg viewBox="0 0 500 150" preserveAspectRatio="none">
+            <path d="M-0.00,49.85 C150.00,149.60 271.37,-49.87 500.00,49.85 L500.00,-0.00 L-0.00,-0.00 Z" />
+          </svg>
+        </Wave>
+      </Banner>
       <Content>
-        <p>oi</p>
+        {
+          // page content
+        }
       </Content>
     </Container>
   );
--- src/pages/Plans/styles.ts
+++ src/pages/Plans/styles.ts
@@ -1,10 +1,81 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
-  height: 100vh;
+export const Container = styled.div``;
 
+export const Header = styled.header`
+  padding: 8px 20px;
+  background: #fff;
+`;
+
+export const HeaderContent = styled.div`
+  max-width: 1120px;
+  margin: 0 auto;
   display: flex;
-  align-items: stretch;
+  align-items: center;
+
+  > img {
+    height: 35px;
+  }
+`;
+
+export const Banner = styled.div`
+  width: 100%;
+  overflow: hidden;
+`;
+
+export const BannerContent = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  background: #1d5297;
+
+  > img {
+    height: 350px;
+    padding: 0px 70px;
+    position: relative;
+    top: 110px;
+  }
+`;
+
+export const BannerText = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  margin-top: 15px;
+
+  span {
+    color: #b9d0ef;
+    line-height: 27px;
+  }
+
+  h2 {
+    color: #ffffff;
+    font-size: 30px;
+    font-weight: bold;
+  }
+
+  svg {
+    color: #ffffff;
+    margin: 0 5px;
+  }
+`;
+
+export const Wave = styled.div`
+  height: 150px;
+  overflow: hidden;
+
+  svg {
+    height: 100%;
+    width: 100%;
+
+    path {
+      stroke: none;
+      fill: #1d5297;
+    }
+  }
 `;
 
 export const Content = styled.div`
