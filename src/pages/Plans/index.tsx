5a43f410330e976fe17a961b2eb05385e1fbf5d7
Thais Pires <tmendespires@gmail.com>
2020-10-26 16:38:12 -0300

    completed cards, radio button, and responsive.

--- src/pages/Plans/index.tsx
+++ src/pages/Plans/index.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
-
+import React, { useCallback, useState } from 'react';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 import { FiCheck } from 'react-icons/fi';
+import { MdInfo } from 'react-icons/md';
 import {
   Container,
   Header,
@@ -10,26 +11,42 @@ import {
   BannerText,
   Wave,
   Content,
+  PaymentPlans,
+  PaymentOption,
   Carrousel,
   Card,
+  CardHeader,
+  CardContent,
+  CardFooter,
   Conditions,
 } from './styles';
 
 import logoImg from '../../assets/hostgator-logo.svg';
 import deskImg from '../../assets/Grupo-29995.svg';
 import customerImg from '../../assets/Grupo-29996.svg';
+import planP from '../../assets/Grupo-29909.svg';
+import planM from '../../assets/Grupo-29910.svg';
+import planTurbo from '../../assets/Grupo-29911.svg';
+import RadioOff from '../../assets/RadioOff.svg';
+import RadioOn from '../../assets/RadioOn.svg';
 
-const SignIn: React.FC = () => {
+const Plans: React.FC = () => {
+  const [paymentPlan, setPaymentPlan] = useState('1');
+  const handlePaymentPlans = useCallback(event => {
+    const { value } = event.target;
+    setPaymentPlan(value);
+  }, []);
+  const isDesktop = useMediaQuery('(min-width:769px)');
   return (
     <Container>
       <Header>
         <HeaderContent>
-          <img src={logoImg} alt="GoBarber" />
+          <img src={logoImg} alt="HostGator" />
         </HeaderContent>
       </Header>
       <Banner>
         <BannerContent>
-          <img src={deskImg} alt="Desk" />
+          {isDesktop && <img src={deskImg} alt="Desk" />}
           <BannerText>
             <span>Hospedagem de Sites</span>
             <h2>
@@ -49,7 +66,7 @@ const SignIn: React.FC = () => {
               Painel de Controle cPanel
             </span>
           </BannerText>
-          <img src={customerImg} alt="Customer" />
+          {isDesktop && <img src={customerImg} alt="Customer" />}
         </BannerContent>
         <Wave>
           <svg viewBox="0 0 500 150" preserveAspectRatio="none">
@@ -58,15 +75,208 @@ const SignIn: React.FC = () => {
         </Wave>
       </Banner>
       <Content>
+        <span>Quero pagar a cada:</span>
+        <PaymentPlans>
+          <PaymentOption htmlFor="3years" paymentPlan={paymentPlan} value="1">
+            {paymentPlan === '1' ? (
+              <img src={RadioOn} alt="Checked Radio Button" />
+            ) : (
+              <img src={RadioOff} alt="Unchecked Radio Button" />
+            )}
+            <span>3 anos</span>
+            <input
+              type="radio"
+              name="paymentPlan"
+              value="1"
+              id="3years"
+              onClick={handlePaymentPlans}
+              defaultChecked
+            />
+          </PaymentOption>
+          <PaymentOption htmlFor="1year" paymentPlan={paymentPlan} value="2">
+            {paymentPlan === '2' ? (
+              <img src={RadioOn} alt="Checked Radio Button" />
+            ) : (
+              <img src={RadioOff} alt="Unchecked Radio Button" />
+            )}
+            <input
+              type="radio"
+              name="paymentPlan"
+              value="2"
+              id="1year"
+              onClick={handlePaymentPlans}
+            />
+            <span>1 ano</span>
+          </PaymentOption>
+          <PaymentOption htmlFor="1month" paymentPlan={paymentPlan} value="3">
+            {paymentPlan === '3' ? (
+              <img src={RadioOn} alt="Checked Radio Button" />
+            ) : (
+              <img src={RadioOff} alt="Unchecked Radio Button" />
+            )}
+            <input
+              type="radio"
+              name="paymentPlan"
+              value="3"
+              id="1month"
+              onClick={handlePaymentPlans}
+            />
+            <span>1 mês</span>
+          </PaymentOption>
+        </PaymentPlans>
         <Carrousel>
           <Card>
-            <span>alou</span>
+            <CardHeader>
+              <img src={planP} alt="Plano P" />
+              <span>Plano P</span>
+            </CardHeader>
+            <CardContent>
+              <span className="text-1">
+                <s>R$ 431,64</s>
+                <strong> R$ 302,15 </strong>
+              </span>
+              <span className="text-1">equivalente a</span>
+              <span className="text-2">
+                <span>R$</span>
+                <strong> 8,39</strong>
+                <span>/mês*</span>
+              </span>
+              <a href="http://google.com">Contrate Agora</a>
+              <strong>
+                1 ano de Domínio Grátis
+                <MdInfo />
+              </strong>
+              <span className="text-3">
+                economize R$ 174,48
+                <span>40% off</span>
+              </span>
+            </CardContent>
+            <CardFooter>
+              <div>
+                <span className="underline">Para 1 site</span>
+              </div>
+              <div>
+                <strong>100 GB</strong>
+                <span>de Armazenamento</span>
+              </div>
+              <div>
+                <span className="underline">
+                  <span>Contas de E-mail</span>
+                  <strong>Ilimitadas</strong>
+                </span>
+              </div>
+              <div>
+                <span>Criador de Sites</span>
+                <strong style={{ textDecoration: 'underline' }}>Grátis</strong>
+              </div>
+              <div>
+                <span>Certificado SSL</span>
+                <strong>Grátis</strong>
+                <span>(https)</span>
+              </div>
+            </CardFooter>
+          </Card>
+          <Card highlighted>
+            <CardHeader>
+              <img src={planM} alt="Plano M" />
+              <span>Plano M</span>
+            </CardHeader>
+            <CardContent>
+              <span className="text-1">
+                <s>R$ 647,64</s>
+                <strong> R$ 453,35 </strong>
+              </span>
+              <span className="text-1">equivalente a</span>
+              <span className="text-2">
+                <span>R$</span>
+                <strong> 12,59</strong>
+                <span>/mês*</span>
+              </span>
+              <a href="http://google.com">Contrate Agora</a>
+              <strong>
+                1 ano de Domínio Grátis
+                <MdInfo />
+              </strong>
+              <span className="text-3">
+                economize R$ 174,48
+                <span>40% off</span>
+              </span>
+            </CardContent>
+            <CardFooter>
+              <div>
+                <span className="underline">Sites Ilimitados</span>
+              </div>
+              <div>
+                <strong>100 GB</strong>
+                <span>de Armazenamento</span>
+              </div>
+              <div>
+                <span className="underline">
+                  <span>Contas de E-mail</span>
+                  <strong>Ilimitadas</strong>
+                </span>
+              </div>
+              <div>
+                <span>Criador de Sites</span>
+                <strong style={{ textDecoration: 'underline' }}>Grátis</strong>
+              </div>
+              <div>
+                <span>Certificado SSL</span>
+                <strong>Grátis</strong>
+                <span>(https)</span>
+              </div>
+            </CardFooter>
           </Card>
           <Card>
-            <span>alou</span>
-          </Card>
-          <Card>
-            <span>alou</span>
+            <CardHeader>
+              <img src={planTurbo} alt="Plano Turbo" />
+              <span>Plano Turbo</span>
+            </CardHeader>
+            <CardContent>
+              <span className="text-1">
+                <s>R$ 1.439,64</s>
+                <strong> R$ 1.007,75 </strong>
+              </span>
+              <span className="text-1">equivalente a</span>
+              <span className="text-2">
+                <span>R$</span>
+                <strong> 27,99</strong>
+                <span>/mês*</span>
+              </span>
+              <a href="http://google.com">Contrate Agora</a>
+              <strong>
+                1 ano de Domínio Grátis
+                <MdInfo />
+              </strong>
+              <span className="text-3">
+                economize R$ 174,48
+                <span>40% off</span>
+              </span>
+            </CardContent>
+            <CardFooter>
+              <div>
+                <span className="underline">Sites Ilimitados</span>
+              </div>
+              <div>
+                <strong>150 GB</strong>
+                <span>de Armazenamento</span>
+              </div>
+              <div>
+                <span className="underline">
+                  <span>Contas de E-mail</span>
+                  <strong>Ilimitadas</strong>
+                </span>
+              </div>
+              <div>
+                <span>Criador de Sites</span>
+                <strong style={{ textDecoration: 'underline' }}>Grátis</strong>
+              </div>
+              <div>
+                <span>Certificado SSL</span>
+                <strong>Grátis</strong>
+                <span>(https)</span>
+              </div>
+            </CardFooter>
           </Card>
         </Carrousel>
         <Conditions>
@@ -77,4 +287,4 @@ const SignIn: React.FC = () => {
   );
 };
 
-export default SignIn;
+export default Plans;
--- src/pages/Plans/styles.ts
+++ src/pages/Plans/styles.ts
@@ -1,4 +1,14 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import { shade } from 'polished';
+
+interface PaymentOptionProps {
+  paymentPlan: string;
+  value: string;
+}
+
+interface CardProps {
+  highlighted?: boolean;
+}
 
 export const Container = styled.div``;
 
@@ -32,7 +42,7 @@ export const BannerContent = styled.div`
 
   > img {
     height: 350px;
-    padding: 0px 70px;
+    padding: 0px 20px;
     position: relative;
     top: 110px;
   }
@@ -89,17 +99,66 @@ export const Content = styled.div`
   width: 100%;
 `;
 
+export const PaymentPlans = styled.div`
+  background-color: #ffffff;
+  border: 1px solid #4480c5;
+  border-radius: 21px;
+  box-shadow: 0px 2px 4px #4480c570;
+  cursor: pointer;
+  float: left;
+  margin-top: 10px;
+  margin-bottom: 50px;
+  &::after {
+    content: '';
+    clear: both;
+    display: table;
+  }
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const PaymentOption = styled.label<PaymentOptionProps>`
+  padding: 10px 20px 10px 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  span {
+    margin-left: 5px;
+    font: normal normal bold 16px/23px Montserrat;
+    letter-spacing: 0px;
+  }
+
+  &:not(:last-child) {
+    border-right: none;
+  }
+
+  input[type='radio']:not(old) {
+    opacity: 0;
+    position: absolute;
+  }
+
+  ${props =>
+    props.paymentPlan === props.value &&
+    css`
+      background-color: #4480c5;
+      border: 1px solid #4480c5;
+      border-radius: 20px;
+      color: #fff;
+    `}
+`;
+
 export const Carrousel = styled.div`
   display: flex;
   flex-direction: row;
   align-items: center;
   justify-content: center;
+  flex-wrap: wrap;
 
   width: 100%;
-  max-width: 1011px;
 `;
 
-export const Card = styled.div`
+export const Card = styled.div<CardProps>`
   background: #fff;
   height: 650px;
   width: 100%;
@@ -109,6 +168,175 @@ export const Card = styled.div`
 
   border: 1px solid #dfecff;
   border-radius: 4px;
+
+  position: relative;
+
+  & + div {
+    margin-bottom: 10px;
+    margin-top: 15px;
+  }
+
+  ${props =>
+    props.highlighted &&
+    css`
+      &::before {
+        content: '';
+        position: absolute;
+        background-color: #ff6a17;
+        width: 100%;
+        height: 10px;
+        top: -9px;
+        border-top-left-radius: 4px;
+        border-top-right-radius: 4px;
+      }
+
+      &::after {
+        content: '';
+        position: absolute;
+        background-color: #ff6a17;
+        width: 100%;
+        height: 5px;
+        bottom: -5px;
+        border-bottom-left-radius: 4px;
+        border-bottom-right-radius: 4px;
+      }
+
+      a {
+        background-color: #ff6a17 !important;
+      }
+    `}
+`;
+
+export const CardHeader = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+
+  border-bottom: 1px solid #dfecff;
+
+  font: normal normal bold 26px/26px Montserrat;
+  letter-spacing: 0px;
+  padding: 40px;
+
+  img {
+    margin-bottom: 10px;
+  }
+`;
+export const CardContent = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+
+  border-bottom: 1px solid #dfecff;
+
+  padding: 20px;
+
+  .text-1 {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    letter-spacing: 0px;
+    color: #333333;
+    font: normal normal normal 13px/19px Montserrat;
+    strong {
+      font: normal normal bold 13px/19px Montserrat;
+    }
+  }
+
+  .text-2 {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    font: normal normal normal 20px/24px Montserrat;
+    letter-spacing: 0px;
+    color: #1d5297;
+    strong {
+      font: normal normal bold 35px/24px Montserrat;
+      color: #1d5297;
+      padding-left: 5px;
+    }
+    padding-top: 5px;
+  }
+
+  a {
+    text-decoration: none;
+    background: #4480c5;
+    border-radius: 26px;
+    height: 52px;
+    color: #fff;
+    text-align: center;
+    font: normal normal bold 22px/27px Montserrat;
+    letter-spacing: 0px;
+    line-height: 52px;
+    width: 100%;
+    margin-top: 30px;
+    margin-bottom: 30px;
+    transition: background-color 0.2s;
+
+    &:hover {
+      background: ${shade(0.2, '#4480C5')};
+    }
+  }
+
+  strong {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font: normal normal bold 15px/20px Montserrat;
+    letter-spacing: 0px;
+    color: #333333;
+    svg {
+      color: #1d5297;
+      margin-left: 5px;
+    }
+  }
+
+  .text-3 {
+    margin: 12px;
+    text-align: left;
+    font: normal normal normal 14px/18px Montserrat;
+    letter-spacing: 0px;
+    color: #1d5297;
+    span {
+      margin-left: 10px;
+      color: #ffffff;
+      text-transform: uppercase;
+      background: #51c99c;
+      border-radius: 26px;
+      padding: 5px 10px;
+      font: normal normal bold 14px/18px Montserrat;
+    }
+  }
+`;
+export const CardFooter = styled.div`
+  padding: 26px;
+  display: flex;
+  justify-content: flex-start;
+  flex-direction: column;
+  text-align: left;
+  font: normal normal normal 16px/32px Montserrat;
+  letter-spacing: 0px;
+  color: #333333;
+
+  strong {
+    font: normal normal bold 16px/32px Montserrat;
+  }
+
+  .underline {
+    border-bottom: 1px #4480c5 dashed;
+  }
+
+  strong + span {
+    margin-left: 3px;
+  }
+
+  span + strong {
+    margin-left: 3px;
+  }
 `;
 
 export const Conditions = styled.div`
